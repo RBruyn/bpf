7c95c54aac475856f1f69bb205436bda5f0f3f9a
Ricardo Bruyn <RBruyn@users.noreply.github.com>
2022-08-17 11:13:24 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+//pagina home
+let time = 3000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#slider img");
+                
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected");
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0;
+
+    images[currentImageIndex]
+        .classList.add("selected");
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time);
+}
+
+window.addEventListener("load", start);
+
+function clicou(){
+    alert('Registration successful');
+}
